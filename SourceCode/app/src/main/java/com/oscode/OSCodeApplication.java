5fda3cceb2af61a9d66571a3293dc995322925d1
WHaiLong <whailong2010@gmail.com>
2016-01-14 00:36:23 +0800

    add DroidPlugin

--- SourceCode/app/src/main/java/com/oscode/OSCodeApplication.java
+++ SourceCode/app/src/main/java/com/oscode/OSCodeApplication.java
@@ -1,8 +1,10 @@
 package com.oscode;
 
 import android.app.Application;
+import android.content.Context;
 
 import com.avos.avoscloud.AVOSCloud;
+import com.morgoo.droidplugin.PluginHelper;
 
 /**
  * Created by whailong on 13/1/16.
@@ -14,5 +16,13 @@ public class OSCodeApplication extends Application {
         super.onCreate();
         // 初始化参数依次为 this, AppId, AppKey
         AVOSCloud.initialize(this, "FCVowRhDwwwHkNKPxAz0rb7L-gzGzoHsz", "3g2B0rtN2ADbU5lpErTASgMq");
+        //这里必须在super.onCreate方法之后，顺序不能变
+        PluginHelper.getInstance().applicationOnCreate(getBaseContext());
+    }
+
+    @Override
+    protected void attachBaseContext(Context base) {
+        PluginHelper.getInstance().applicationAttachBaseContext(base);
+        super.attachBaseContext(base);
     }
 }
