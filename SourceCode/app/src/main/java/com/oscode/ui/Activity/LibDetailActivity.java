0547ca5f85271ce36083337b18ec883f5967cbbc
WHaiLong <whailong2010@gmail.com>
2016-01-27 00:08:11 +0800

    Optimize Detail page layout

--- SourceCode/app/src/main/java/com/oscode/ui/Activity/LibDetailActivity.java
+++ SourceCode/app/src/main/java/com/oscode/ui/Activity/LibDetailActivity.java
@@ -1,24 +1,16 @@
 package com.oscode.ui.Activity;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.os.RemoteException;
+import android.support.design.widget.CollapsingToolbarLayout;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
+import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 
-import com.avos.avoscloud.AVException;
-import com.avos.avoscloud.GetDataCallback;
-import com.avos.avoscloud.ProgressCallback;
-import com.morgoo.droidplugin.pm.PluginManager;
 import com.oscode.R;
 import com.oscode.model.OSCodeLib;
-
-import org.kymjs.kjframe.utils.FileUtils;
-
-import java.io.File;
 
 
 /**
@@ -36,6 +28,7 @@ public class LibDetailActivity extends AppCompatActivity implements View.OnClick
     private TextView tvLastUpdate;
     private TextView tvAuthor;
     private TextView tvLicense;
+    private ImageView ivHeader;
 
     private OSCodeLib codeLib;
 
@@ -43,31 +36,44 @@ public class LibDetailActivity extends AppCompatActivity implements View.OnClick
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_libdetail);
-        btnCollect = (Button) findViewById(R.id.btn_collect);
-        btnDownload = (Button) findViewById(R.id.btn_download);
-        btnShare = (Button) findViewById(R.id.btn_share);
+        final Toolbar toolbar = (Toolbar) findViewById(R.id.anim_toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
+//        btnCollect = (Button) findViewById(R.id.btn_collect);
+//        btnDownload = (Button) findViewById(R.id.btn_download);
+//        btnShare = (Button) findViewById(R.id.btn_share);
+        ivHeader = (ImageView) findViewById(R.id.iv_header);
         tvDescription = (TextView) findViewById(R.id.tv_lib_discription);
         tvGithubAddress = (TextView) findViewById(R.id.tv_githubAddress);
         tvVersion = (TextView) findViewById(R.id.tv_version);
-        tvLibName = (TextView) findViewById(R.id.tv_libName);
         tvAuthor = (TextView) findViewById(R.id.tv_author);
         tvLicense = (TextView) findViewById(R.id.tv_license);
         tvLastUpdate = (TextView) findViewById(R.id.tv_LastUpdate);
 
-        btnCollect.setOnClickListener(this);
-        btnDownload.setOnClickListener(this);
-        btnShare.setOnClickListener(this);
+//        btnCollect.setOnClickListener(this);
+//        btnDownload.setOnClickListener(this);
+//        btnShare.setOnClickListener(this);
 
         codeLib = getIntent().getParcelableExtra("selectedItem");
 
-        tvLibName.setText(codeLib.getLibName());
+        CollapsingToolbarLayout collapsingToolbar = (CollapsingToolbarLayout) findViewById(R.id.collapsing_toolbar);
+        collapsingToolbar.setTitle(codeLib.getLibName());
+
         tvDescription.setText(codeLib.getDescriptionCN());
         tvGithubAddress.setText(codeLib.getGithubAddress());
         tvVersion.setText(codeLib.getMinSDKVersion());
         tvAuthor.setText(codeLib.getAuthor());
         tvLicense.setText(codeLib.getLicense());
-        btnDownload.setText("下载(" + codeLib.getSize() + "MB)");
+//        btnDownload.setText("下载(" + codeLib.getSize() + "MB)");
+
+//        codeLib.getLibPreImage().getDataInBackground(new GetDataCallback() {
+//            @Override
+//            public void done(byte[] bytes, AVException e) {
+//                Bitmap bm = BitmapFactory.decodeByteArray(bytes,0,bytes.length);
+//                LibDetailActivity.this.ivHeader.setImageBitmap(bm);
+//            }
+//        });
 
         codeLib.increaseViewCount();
         codeLib.saveInBackground();
@@ -76,50 +82,50 @@ public class LibDetailActivity extends AppCompatActivity implements View.OnClick
     @Override
     public void onClick(View v) {
         switch (v.getId()) {
-            case R.id.btn_collect:
+//            case R.id.btn_collect:
 
-                break;
-
-            case R.id.btn_download:
-
-                codeLib.getLibApkFile().getDataInBackground(new GetDataCallback() {
-                    @Override
-                    public void done(byte[] bytes, AVException e) {
-                        Log.v("Done--->", "____>");
-                        String fileFolder = FileUtils.getSDCardPath() + File.separator + "OSCode" + File.separator;
-                        String fileName = "lib.apk";
-
-                        FileUtils.saveFileCache(bytes, FileUtils.getSDCardPath() + File.separator + "OSCode" + File.separator, "lib.apk");
-//                        int PluginManager.getInstance().installPackage(String filepath, int flags);
-                        try {
-                            PluginManager.getInstance().installPackage(fileFolder + fileName, 0);
-                        } catch (RemoteException e1) {
-                            e1.printStackTrace();
-                        }
-                    }
-                }, new ProgressCallback() {
-                    @Override
-                    public void done(Integer integer) {
-                        btnDownload.setText("" + integer);
-                    }
-                });
-
-                break;
-
-            case R.id.btn_share:
-
-                String fileFolder = FileUtils.getSDCardPath() + File.separator + "OSCode" + File.separator;
-                String fileName = "lib.apk";
-
-                try {
-                    int ints = PluginManager.getInstance().installPackage(fileFolder + fileName, 0);
-                    Log.v("tag--->", "-->" + ints);
-                } catch (RemoteException e1) {
-                    e1.printStackTrace();
-                }
-                Intent intent = new Intent().setClassName(this, "com.daimajia.slider.demo.MainActivity");
-                this.startActivity(intent);
-                break;
+//                break;
+//
+//            case R.id.btn_download:
+//
+//                codeLib.getLibApkFile().getDataInBackground(new GetDataCallback() {
+//                    @Override
+//                    public void done(byte[] bytes, AVException e) {
+//                        Log.v("Done--->", "____>");
+//                        String fileFolder = FileUtils.getSDCardPath() + File.separator + "OSCode" + File.separator;
+//                        String fileName = "lib.apk";
+//
+//                        FileUtils.saveFileCache(bytes, FileUtils.getSDCardPath() + File.separator + "OSCode" + File.separator, "lib.apk");
+////                        int PluginManager.getInstance().installPackage(String filepath, int flags);
+//                        try {
+//                            PluginManager.getInstance().installPackage(fileFolder + fileName, 0);
+//                        } catch (RemoteException e1) {
+//                            e1.printStackTrace();
+//                        }
+//                    }
+//                }, new ProgressCallback() {
+//                    @Override
+//                    public void done(Integer integer) {
+//                        btnDownload.setText("" + integer);
+//                    }
+//                });
+//
+//                break;
+//
+//            case R.id.btn_share:
+//
+//                String fileFolder = FileUtils.getSDCardPath() + File.separator + "OSCode" + File.separator;
+//                String fileName = "lib.apk";
+//
+//                try {
+//                    int ints = PluginManager.getInstance().installPackage(fileFolder + fileName, 0);
+//                    Log.v("tag--->", "-->" + ints);
+//                } catch (RemoteException e1) {
+//                    e1.printStackTrace();
+//                }
+//                Intent intent = new Intent().setClassName(this, "com.daimajia.slider.demo.MainActivity");
+//                this.startActivity(intent);
+//                break;
         }
     }
 
