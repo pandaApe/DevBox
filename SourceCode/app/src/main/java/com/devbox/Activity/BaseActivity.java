de1ef86aad99d861bcb6f918e90eba186c3838a1
WHaiLong <whailong2010@gmail.com>
2016-02-24 00:02:07 +0800

    optimize BaseAcitvity

--- SourceCode/app/src/main/java/com/devbox/Activity/BaseActivity.java
+++ SourceCode/app/src/main/java/com/devbox/Activity/BaseActivity.java
@@ -1,17 +1,17 @@
 package com.devbox.Activity;
 
 import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.CoordinatorLayout;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.LayoutInflater;
 import android.view.MotionEvent;
 import android.view.View;
-import android.view.ViewGroup;
 
 import com.devbox.DBApplication;
 import com.devbox.R;
+import com.devbox.utils.LogUtil;
 
 public class BaseActivity extends AppCompatActivity {
 
@@ -19,57 +19,44 @@ public class BaseActivity extends AppCompatActivity {
     protected DBApplication application;
     protected Toolbar toolbar;
     private android.support.v7.app.ActionBar mActionBar;
-    private CoordinatorLayout coordinatorLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+        LogUtil.log(this, "---------------------OnCreate");
+
+        this.initVariables();
+        this.initViews(savedInstanceState);
+        this.loadData();
+        this.registerBordcast();
+    }
+
+    protected void initVariables() {
         this.context = getApplicationContext();
         this.application = (DBApplication) this.getApplication();
-
-//        initContentView(R.layout.activity_base);
     }
 
-
-    private void initContentView(int layoutResID) {
-        ViewGroup viewGroup = (ViewGroup) findViewById(android.R.id.content);
-        viewGroup.removeAllViews();
-
-        LayoutInflater.from(this).inflate(layoutResID, viewGroup, true);
-        coordinatorLayout = (CoordinatorLayout) findViewById(R.id.root_corrdinatorlayout);
+    //The method setContentView: should be called in this method.
+    protected void initViews(Bundle savedInstanceState) {
     }
 
+    protected void loadData() {
+    }
+
+    protected void registerBordcast() {
+    }
 
     @Override
     public void setContentView(int layoutResID) {
         super.setContentView(layoutResID);
-
-//        LayoutInflater.from(this).inflate(layoutResID, coordinatorLayout, true);
-
         initToolbar();
     }
 
     private void initToolbar() {
-
         toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         this.mActionBar = getSupportActionBar();
-    }
-
-
-    @Override
-    public void setContentView(View view) {
-        super.setContentView(view);
-//        coordinatorLayout.addView(view);
-
-        initToolbar();
-    }
-
-    @Override
-    public void setContentView(View view, ViewGroup.LayoutParams params) {
-        super.setContentView(view, params);
-        initToolbar();
     }
 
     @Override
@@ -84,20 +71,51 @@ public class BaseActivity extends AppCompatActivity {
     }
 
     protected void setToolbarTitle(CharSequence text) {
-
         this.mActionBar.setTitle(text);
     }
 
     @Override
     protected void onResume() {
+        LogUtil.log(this, "---------------------onResume");
         super.onResume();
 //        Bugtags.onResume(this);
     }
 
     @Override
     protected void onPause() {
+        LogUtil.log(this, "---------------------onPause");
         super.onPause();
+
 //        Bugtags.onPause(this);
+    }
+
+    @Override
+    protected void onStop() {
+        LogUtil.log(this, "---------------------onStop");
+        super.onStop();
+    }
+
+    @Override
+    protected void onDestroy() {
+        LogUtil.log(this, "---------------------onDestroy");
+        super.onDestroy();
+    }
+
+
+    public void showSnackbar(String msg) {
+        View rootView = findViewById(android.R.id.content);
+        if (rootView != null)
+            Snackbar.make(rootView, msg, Snackbar.LENGTH_LONG).show();
+    }
+
+    protected void showActivity(Intent intent) {
+        this.startActivity(intent);
+    }
+
+    protected void showActivity(Class<?> cls) {
+        Intent intent = new Intent();
+        intent.setClass(this, cls);
+        this.startActivity(intent);
     }
 
 }
