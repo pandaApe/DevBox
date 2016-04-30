15e6f2ae5f93e185187111ef520841a017759b8f
WHaiLong <whailong2010@gmail.com>
2016-04-30 21:57:56 +0800

    Added dependency injection class

---
+++ app/src/main/java/com/hl/devbox/di/components/MainAtyComponent.java
@@ -0,0 +1,13 @@
+package com.hl.devbox.di.components;
+
+/**
+ * Description:
+ *
+ * @Author: PandaApe.
+ * @CreatedAt: 30/4/16 21:42.
+ * @Email: whailong2010@gmail.com
+ */
+public interface MainAtyComponent {
+
+
+}
---
+++ app/src/main/java/com/hl/devbox/di/modules/MainAtyModule.java
@@ -0,0 +1,15 @@
+package com.hl.devbox.di.modules;
+
+/**
+ * Description:
+ *
+ * @Author: PandaApe.
+ * @CreatedAt: 30/4/16 21:43.
+ * @Email: whailong2010@gmail.com
+ */
+public class MainAtyModule {
+
+//    @Provides
+//    public
+
+}
