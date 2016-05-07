b16e13cae3439cf44d137a7d884ad26d3994b216
WHaiLong <whailong2010@gmail.com>
2016-05-07 11:24:54 +0800

    Created ListItemOnClickListener class for adapter deliver click event.

---
+++ app/src/main/java/com/pa/devbox/ui/delegate/ListItemOnClickListener.java
@@ -0,0 +1,11 @@
+package com.pa.devbox.ui.delegate;
+
+/**
+ * Description:
+ * <p>
+ * Author: PandaApe.
+ * CreatedAt: 7/5/16 11:23.
+ * Email: whailong2010@gmail.com
+ */
+public interface ListItemOnClickListener {
+}
